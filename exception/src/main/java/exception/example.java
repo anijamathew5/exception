2f3c6a025648764051fc2b7e480559ad5db81edd
ASUS <ASUS@LAPTOP-4TVV0GHV>
2023-09-28 18:47:59 +0530

    commit

---
+++ exception/src/main/java/exception/example.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		int c=a+b;
+		System.out.println(c);
+
+	}
+
+}
